2adc5f6614aa209637212e38cc1d79fe7793e659
Longopy <javier_longobardo@hotmail.com>
2025-03-16 00:15:03 +0100

    Replace github workflow with vite docs and modify vite base name

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from "vite";
 import vitePluginFaviconsInject from "vite-plugin-favicons-inject";
 
 export default defineConfig({
-  base: "/exfil/",
+  base: "/ExFil/",
   build: {
     outDir: "build",
     rollupOptions: {
